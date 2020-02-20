10d3186ccaa2a29c064ab0e0f6f3d0917eb55b47
MishaVyuzh <vyuzzzh@yandex.ru>
2020-02-20 21:15:14 +0300

    ret /reg

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,12 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Counter-likes' });
 });
 
-router.get('/:code', (req, res, next) => {
+router.get('/reg', (req, res, next) => {
+  console.log('fasdfasf');
+  
   console.log(req.query.code);
 
-  res.render('login');
+  res.render('test');
 });
 
 
