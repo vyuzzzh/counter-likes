0bd715188766c2870d5c0bf0468e29ef485ee3a2
MishaVyuzh <vyuzzzh@yandex.ru>
2020-02-20 22:03:16 +0300

    del res from /req

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,17 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Counter-likes' });
 });
 
-router.get('/reg', async (req, res, next) => {
+router.get('/reg', (req, res, next) => {
   const { code } = req.query;
   console.log(code);
   const url = `https://oauth.vk.com/access_token?client_id=1&client_secret=H2Pk8htyFD8024mZaPHm&redirect_uri=https://counter-likes.herokuapp.com/reg/&code=${code}`;
 
-  await request(url, (error, response, body) => {
+  request(url, (error, response, body) => {
     console.error('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
+    res.redirect('/');
   });
-
-  res.render('test');
 });
 
 
