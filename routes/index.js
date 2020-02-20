1886ff0bf22e2b7d6490e00af95382ccac7c563a
MishaVyuzh <vyuzzzh@yandex.ru>
2020-02-20 20:53:25 +0300

    add code

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,11 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Counter-likes' });
 });
 
+router.get('/:code', (req, res, next) => {
+  console.log(req.query.code);
+
+  res.render('login');
+});
+
+
 module.exports = router;
