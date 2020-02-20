861db0c77bf027ec44e55c0d43234c5ab8cd1a34
MishaVyuzh <vyuzzzh@yandex.ru>
2020-02-20 21:44:28 +0300

    add url

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,10 @@ router.get('/', (req, res, next) => {
 
 router.get('/reg', (req, res, next) => {
   const { code } = req.query;
+  console.log(code);
+  const url = `https://oauth.vk.com/access_token?client_id=1&client_secret=H2Pk8htyFD8024mZaPHm&redirect_uri=https://counter-likes.herokuapp.com/reg/&code=${code}`;
 
-  request(`https://oauth.vk.com/access_token?client_id=1&client_secret=H2Pk8htyFD8024mZaPHm&redirect_uri=https://counter-likes.herokuapp.com/reg/&code=${code}`, (error, response, body) => {
+  request(url, (error, response, body) => {
     console.error('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
