dd9fe6053219c855b36795d56389bd50d41eb18b
MishaVyuzh <vyuzzzh@yandex.ru>
2020-02-21 02:07:41 +0300

    move route /reg to /auth/reg

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,34 @@
 const router = require('express').Router();
-// const User = require('../models/user');
+const rp = require('request-promise');
+const User = require('../models/user');
 
 router.get('/login', (req, res) => {
   res.render('login');
+});
+
+router.get('/reg', (req, res, next) => {
+  const { code } = req.query;
+  console.log(`CODE>>>>>>>>>>>>>>${code}`);
+  const url = `https://oauth.vk.com/access_token?client_id=7327798&client_secret=KVv5burkbNpWD5qLKyIt&redirect_uri=https://counter-likes.herokuapp.com/reg/&code=${code}`;
+  console.log(`URL>>>>>>>>>>>>>>>${url}`);
+
+  rp(url)
+    .then((objInfo) => {
+      console.log(objInfo);
+      const {
+        access_token, user_id, email,
+      } = objInfo;
+      User.create({ user_id, email }, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      req.session.user = user._id;
+      res.redirect('/');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 router.get('/logout', (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,10 @@
 const express = require('express');
-const rp = require('request-promise');
-const User = require('../models/user');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Counter-likes' });
-});
-
-router.get('/reg', (req, res, next) => {
-  const { code } = req.query;
-  console.log(`CODE>>>>>>>>>>>>>>${code}`);
-  const url = `https://oauth.vk.com/access_token?client_id=7327798&client_secret=KVv5burkbNpWD5qLKyIt&redirect_uri=https://counter-likes.herokuapp.com/reg/&code=${code}`;
-  console.log(`URL>>>>>>>>>>>>>>>${url}`);
-
-  rp(url)
-    .then((objInfo) => {
-      console.log(objInfo);
-      const {
-        access_token, user_id, email,
-      } = objInfo;
-      User.create({ user_id, email }, (err) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-      req.session.user = user._id;
-      res.redirect('/');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 });
 
 module.exports = router;
