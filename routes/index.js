e72357cca2f5641163ee76a38f98f9466e0b72a0
MishaVyuzh <vyuzzzh@yandex.ru>
2020-02-20 21:40:34 +0300

    add request

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+
 
 const router = express.Router();
 
@@ -8,9 +10,13 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/reg', (req, res, next) => {
-  console.log('fasdfasf');
-  
-  console.log(req.query.code);
+  const { code } = req.query;
+
+  request(`https://oauth.vk.com/access_token?client_id=1&client_secret=H2Pk8htyFD8024mZaPHm&redirect_uri=https://counter-likes.herokuapp.com/reg/&code=${code}`, (error, response, body) => {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
 
   res.render('test');
 });
