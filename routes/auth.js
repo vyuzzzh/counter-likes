6bbc0568f7bfdcc77b2214e1076b75d72d200bf8
MishaVyuzh <vyuzzzh@yandex.ru>
2020-02-21 09:39:12 +0300

    move reg from / to auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const rp = require('request-promise');
+
 
 router.get('/login', (req, res) => {
   res.render('login');
@@ -12,4 +14,21 @@ router.get('/logout', (req, res) => {
   });
 });
 
+router.get('/reg', (req, res, next) => {
+  const { code } = req.query;
+  console.log(`CODE>>>>>>>>>>>>>>${code}`);
+  const url = `https://oauth.vk.com/access_token?client_id=7327798&client_secret=KVv5burkbNpWD5qLKyIt&redirect_uri=https://counter-likes.herokuapp.com/auth/reg/&code=${code}`;
+  console.log(`URL>>>>>>>>>>>>>>>${url}`);
+
+
+  rp(url)
+    .then((objInfo) => {
+      console.log('>>>>>>>>>>>>>>>');
+      console.log(objInfo);
+      res.redirect('/');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 module.exports = router;
