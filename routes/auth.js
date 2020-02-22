85e7c5e257f8dda7763cabbd5d4309eb65b3b3a3
MishaVyuzh <vyuzzzh@yandex.ru>
2020-02-23 01:28:31 +0300

    check user finded

--- routes/auth.js
+++ routes/auth.js
@@ -16,34 +16,18 @@ router.get('/logout', (req, res) => {
 });
 
 router.get('/reg', (req, res, next) => {
-  console.log('>>>>>>>>>>>>>>>>>>>>>>REG>>>>>>>>>>>>>>>');
-  
   const { code } = req.query;
-  console.log('>>>>>>>>>>>>>>>>>>>>>>REG>>>>>>>>>>>>>>>');
-
-  console.log(code);
-  
   const url = `https://oauth.vk.com/access_token?client_id=7327798&client_secret=d60Ug4amPVS7N2qlGBoq&redirect_uri=http://counter-likes.herokuapp.com/auth/reg&code=${code}`;
-  
-  console.log(`>>>>>>>URL>>>>${url}`);
-  
   rp(url)
     .then(async (objInfo) => {
-      console.log('gagjagkajglkralgrje');
-      console.log(`>>>>>>>>>>>${objInfo}`);
-      
       const info = JSON.parse(objInfo);
-      console.log(`INFOO>>>>${info}`);
-      
       const id = info.user_id;
       const { email, access_token, expires_in } = info;
-      console.log(id, email, access_token);
-      
       const userFind = await User.findOne({ id });
+      console.log(`USER FINDED ${userFind}`);
 
-      console.log('<<<<<<<<WTF???');
       if (!userFind) {
-        
+        console.log('<<<<<<<<CREATE USER');
         await User.create({
           id, email, access_token, expires_in,
         });
